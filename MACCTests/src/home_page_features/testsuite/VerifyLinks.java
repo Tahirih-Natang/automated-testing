3eecebf75cfd84990061ef591e1ff44a1b06b8e9
Tahirih-Natang <krysnu@gmail.com>
2016-06-13 10:01:07 +0100

    Refactored VerifyLinks class

--- MACCTests/src/home_page_features/testsuite/VerifyLinks.java
+++ MACCTests/src/home_page_features/testsuite/VerifyLinks.java
@@ -7,65 +7,60 @@ package home_page_features.testsuite;
  * @author tahirih-natang
  *
  */
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import common.DataProviderCommonCode;
 
-public class VerifyLinks {
-
-	public static void main(String[] args) 
-	{
-		WebDriver driver=new FirefoxDriver();
-		
-		driver.manage().window().maximize();
-		
-		driver.get("http://localhost:8001");
-		
-		List<WebElement> links=driver.findElements(By.tagName("a"));
-		
-		System.out.println("Total links are "+links.size());
-		
-		for(int i=0;i<links.size();i++)
-		{
-			
-			WebElement ele= links.get(i);
-			
-			String url=ele.getAttribute("href");
-			
-			verifyLinkActive(url);
-			
-		}
-		
-	}
+public class VerifyLinks extends DataProviderCommonCode {
 	
-	public static void verifyLinkActive(String linkUrl)
-	{
-        try 
-        {
-           URL url = new URL(linkUrl);
-           
-           HttpURLConnection httpURLConnect=(HttpURLConnection)url.openConnection();
-           
-           httpURLConnect.setConnectTimeout(3000);
-           
-           httpURLConnect.connect();
-           
-           if(httpURLConnect.getResponseCode()==200)
-           {
-               System.out.println(linkUrl+" - "+httpURLConnect.getResponseMessage());
-            }
-          if(httpURLConnect.getResponseCode()==HttpURLConnection.HTTP_NOT_FOUND)  
-           {
-               System.out.println(linkUrl+" - "+httpURLConnect.getResponseMessage() + " - "+ HttpURLConnection.HTTP_NOT_FOUND);
-            }
-        } catch (Exception e) {
-           
-        }
-    } 
+	private int invalidLinksCount;
 
+	@Test
+	public void validateInvalidLinks() {
+
+		try {
+			invalidLinksCount = 0;
+			List<WebElement> anchorTagsList = driver.findElements(By
+					.tagName("a"));
+			System.out.println("Total no. of links are "
+					+ anchorTagsList.size());
+			for (WebElement anchorTagElement : anchorTagsList) {
+				if (anchorTagElement != null) {
+					String url = anchorTagElement.getAttribute("href");
+					if (url != null && !url.contains("javascript")) {
+						verifyURLStatus(url);
+					} else {
+						invalidLinksCount++;
+					}
+				}
+			}
+
+			System.out.println("Total no. of invalid links are "
+					+ invalidLinksCount);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void verifyURLStatus(String URL) {
+
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(URL);
+		try {
+			HttpResponse response = client.execute(request);
+			if (response.getStatusLine().getStatusCode() != 200)
+				invalidLinksCount++;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
